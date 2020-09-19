f28bf27d56bd7bddcba90a58bb4b5dde353f993b
LATraber <traber73@yandex.ru>
2020-09-20 00:02:05 +0500

    Сделал интерфейс с помощью WindowsForms.

---
+++ CaesarCipher/CaesarCipher/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CaesarCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textInputField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textOutputField = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textInputField
+            // 
+            this.textInputField.Location = new System.Drawing.Point(12, 26);
+            this.textInputField.Multiline = true;
+            this.textInputField.Name = "textInputField";
+            this.textInputField.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textInputField.Size = new System.Drawing.Size(785, 156);
+            this.textInputField.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Вход для текста";
+            // 
+            // textOutputField
+            // 
+            this.textOutputField.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textOutputField.Location = new System.Drawing.Point(12, 241);
+            this.textOutputField.Multiline = true;
+            this.textOutputField.Name = "textOutputField";
+            this.textOutputField.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textOutputField.Size = new System.Drawing.Size(785, 197);
+            this.textOutputField.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(664, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Сделать сдвиг";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(421, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(13, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(421, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textOutputField);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textInputField);
+            this.Name = "Form1";
+            this.Text = "Шифр Цезаря";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textInputField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textOutputField;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
