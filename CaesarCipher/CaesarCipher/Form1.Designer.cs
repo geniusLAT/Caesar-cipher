0b4cb948c5f1b033facce357cda478d3a245fb66
LATraber <traber73@yandex.ru>
2020-09-20 15:48:49 +0500

    Добавил основной функционал. Теперь при нажатии на кнопку "Сделать сдвиг" в нижнем поле появляется зашифрованное сообщение. Обычные символы кириллицы шифруются. Заглавные сначала приводятся к строчным, а потом шифруются. Все символы не шифруются.
    Сделал визуализацию, где сопоставляются буквы оригинала и шифра.

--- CaesarCipher/CaesarCipher/Form1.Designer.cs
+++ CaesarCipher/CaesarCipher/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textInputField
@@ -72,6 +73,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "Сделать сдвиг";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
@@ -82,7 +84,6 @@
             this.label2.Size = new System.Drawing.Size(421, 25);
             this.label2.TabIndex = 4;
             this.label2.Text = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
@@ -94,12 +95,24 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
             // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(614, 188);
+            this.textBox1.MaxLength = 3;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(44, 31);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "0";
+            this.textBox1.Validated += new System.EventHandler(this.textBox1_Validated);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
@@ -121,6 +134,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- CaesarCipher/CaesarCipher/Form1.cs
+++ CaesarCipher/CaesarCipher/Form1.cs
@@ -8,15 +8,81 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace CaesarCipher
 {
+
+
     public partial class Form1 : Form
     {
+        const string AL = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        string F = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        int key = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
-        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textOutputField.Text = Process(textInputField.Text);
+        }
+
+        string Process(string original)
+        {
+            string NewOne = "";
+            original = original.ToLower();
+            for (int i = 0; i < original.Length; i++)
+            {
+                int AlNum = -1;
+                for(int z = 0; z < 33; z++)
+                {
+                    if (original[i] == AL[z])
+                    {
+                        AlNum = z;
+                        break;
+                    }
+                }
+                if (AlNum == -1)
+                {
+                    NewOne += original[i];
+                }
+                else
+                {
+                    NewOne += F[AlNum];
+                }
+                
+            }
+            return NewOne;
+        }
+
+        private void textBox1_Validated(object sender, EventArgs e)//Игрок ввёл новое число в поле ключа
+        {
+            int L = key;
+            try
+            {
+                L = Convert.ToInt32(textBox1.Text);
+                if (Math.Abs(L) > 32) L = L % 32;
+                key = L;
+            }
+            catch (System.FormatException)//Вызывается, если игрок пытается ввести что-то, что не получится преобразить к int
+            {
+                //textBox1.Text = key.ToString();
+
+            }
+            textBox1.Text = key.ToString();
+
+            
+            F = "";
+            for (int i = 0; i < 33; i++)
+            {
+
+                int g = i + key;
+                if (g > 32) g -= 33; if (g < 0) g += 33;
+                F += AL[g];
+            }
+            label3.Text = F;
+        }
     }
 }
